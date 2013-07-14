555ae5b88dd1a11c47403b3ed0094416542afcc2
Stuart Liston <stuart.liston@gmail.com>
2013-07-14 14:19:22 +1000

    Tidying up tests for date diff class

--- spec/date_challenge/date_difference_in_days_spec.rb
+++ spec/date_challenge/date_difference_in_days_spec.rb
@@ -3,15 +3,15 @@ require 'date_challenge/date_difference_in_days'
 module DateChallenge
   describe DateDifferenceInDays do
 
+    # Would be cool if the order of dates didn't matter
+
     describe '#to_i' do
 
       it 'is the difference in days' do
-        earliest_date = double(year: 2012, month: 05, day: 01)
-        latest_date   = double(year: 2013, month: 01, day: 10)
-        DateDifferenceInDays.new(earliest_date, latest_date).to_i.should eq 375
+        earliest_date = double(year: 2004, month: 01, day: 01)
+        latest_date   = double(year: 2005, month: 05, day: 11)
+        expect(DateDifferenceInDays.new(earliest_date, latest_date).to_i).to eq 496 # 366 + 120 + 10
       end
-
-      # TODO: Edge cases
 
     end
 
