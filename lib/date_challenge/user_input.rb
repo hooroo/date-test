d15928b26d9599a5e53b6bb655fad3c2a6949e67
Stuart Liston <stuart.liston@gmail.com>
2013-07-14 13:45:55 +1000

    Building out user input class

--- lib/date_challenge/user_input.rb
+++ lib/date_challenge/user_input.rb
@@ -3,16 +3,18 @@ require_relative 'date'
 module DateChallenge
   class UserInput
 
+    FORMAT = /\d{2}\s\d{2}\s\d{4},{1}\s\d{2}\s\d{2}\s\d{4}/
+
     def initialize(input)
       @input = input
     end
 
-    # TODO: Implement!
     def valid?
-      raise NotImplementedError
+      input =~ FORMAT
     end
 
     def dates
+      return [] unless valid?
       parts.collect { |date| Date.parse(date) }
     end
 
--- spec/date_challenge/user_input_spec.rb
+++ spec/date_challenge/user_input_spec.rb
@@ -1,9 +1,36 @@
-require 'user_input'
+require 'date_challenge/user_input'
 
-describe UserInput do
+module DateChallenge
+  describe UserInput do
 
-  describe '#first_date' do
-    it ''
+    describe '#valid?' do
+
+      it 'demands a format of DD MM YYYY, DD MM YYYY' do
+        expect(UserInput.new('bacon!').valid?).to be_false
+        expect(UserInput.new('01 01 2000 02 03 2010').valid?).to be_false
+        expect(UserInput.new('01 01 2000, 02 03 201').valid?).to be_false
+        expect(UserInput.new('10 01 2013, 05 12 2004').valid?).to be_true
+      end
+
+    end
+
+    describe '#dates' do
+
+      it 'is an array of correctly-parsed dates' do
+        input = UserInput.new('10 01 2013, 05 12 2004')
+        expect(input.dates).to eq [ Date.new(2013, 1, 10), Date.new(2004, 12, 5) ]
+      end
+
+      context 'when the input is invalid' do
+
+        it 'is empty' do
+          input = UserInput.new('irrelevant')
+          input.stub(:valid? => false)
+          expect(input.dates).to be_empty
+        end
+      end
+
+    end
+
   end
-
 end
